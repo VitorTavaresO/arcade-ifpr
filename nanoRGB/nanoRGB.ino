10101e94dc27cd42bb0f2e6d46b9cede30ad42ad
VitorTavaresO <vitortavares.o55@gmail.com>
2023-07-12 12:46:10 -0300

    Potentiometer

--- nanoRGB/nanoRGB.ino
+++ nanoRGB/nanoRGB.ino
@@ -1,8 +1,15 @@
- const int pinRed = 11;
- const int pinGreen = 10;
- const int pinBlue = 9;
+const int pinRed = 11;
+const int pinGreen = 10;
+const int pinBlue = 9;
+const int pinPotRed = A0;
+const int pinPotGreen = A1;
+const int pinPotBlue = A2;
 
 void setup() {
+  pinMode(pinPotRed, INPUT);
+  pinMode(pinPotGreen, INPUT);
+  pinMode(pinPotBlue, INPUT);
+
   pinMode(pinRed, OUTPUT);
   pinMode(pinGreen, OUTPUT);
   pinMode(pinBlue, OUTPUT);
@@ -10,8 +17,11 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(pinRed, 255);
-  digitalWrite(pinGreen, 255);
-  digitalWrite(pinBlue, 0);
+  int redValue = map(analogRead(pinPotRed), 0, 1023, 0, 255);
+  int greenValue = map(analogRead(pinPotGreen), 0, 1023, 0, 255);
+  int blueValue = map(analogRead(pinPotBlue), 0, 1023, 0, 255);
+  digitalWrite(pinRed, redValue);
+  digitalWrite(pinGreen, greenValue);
+  digitalWrite(pinBlue, blueValue);
 
 }
