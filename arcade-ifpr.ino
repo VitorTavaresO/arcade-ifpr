40efb8357ce4c9eb569348653d75f0bf6a807607
Vitor Tavares <vitortavares.o55@gmail.com>
2023-12-04 17:52:09 -0300

    Mouse Full Working

--- arcade-ifpr.ino
+++ arcade-ifpr.ino
@@ -3,6 +3,7 @@
 
 const int joystickX = A0;
 const int joystickY = A1;
+const int joystickButton = 15;
 
 int cursorSpeed = 10;
 int responseDelay = 5;
@@ -11,17 +12,24 @@ int center = cursorSpeed/2;
 
 
 void setup() {
+    pinMode(joystickButton, INPUT_PULLUP);
     Mouse.begin();
     Keyboard.begin();
+    Serial.begin(115200);
 }
 
 void loop() {
-  int xReading = readAxis(joystickX);
-  int yReading = readAxis(joystickY);
+    int xReading = readAxis(joystickX);
+    int yReading = readAxis(joystickY);
 
     Mouse.move(xReading, yReading, 0);
+    if(digitalRead(joystickButton) == LOW) {
+        Mouse.press(MOUSE_LEFT);
+    } else {
+        Mouse.release(MOUSE_LEFT);
+    }
 
-  delay(responseDelay);
+    delay(responseDelay);
 }
 
 
