0790a5557124f15fe5e8c1c8496abd2488d966a3
Vitor Tavares <vitortavares.o55@gmail.com>
2023-12-20 12:27:22 -0300

    Revert Commit

--- arcade-ifpr.ino
+++ arcade-ifpr.ino
@@ -1,6 +1,5 @@
 #include <Mouse.h>
 #include <Keyboard.h>
-#include "functions.h"
 
 const int joystickX = A0;
 const int joystickY = A1;
@@ -43,4 +42,18 @@ void loop() {
     }
 
     delay(responseDelay);
+}
+
+int readAxis(int thisAxis, int cursorSpeed, int threshold, int center) {
+  int reading = analogRead(thisAxis);
+
+  reading = map(reading, 0, 1023, 0, cursorSpeed);
+
+  int distance = reading - center;
+
+  if (abs(distance) < threshold) {
+    distance = 0;
+  }
+
+  return distance;
 }
--- functions.h
+++
@@ -1,4 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-int readAxis(int thisAxis, int cursorSpeed, int threshold, int center);
-#endif
