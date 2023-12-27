63e987d76d25b90809ec72ed0d0642e601a5d817
Vitor Tavares <vitortavares.o55@gmail.com>
2023-12-27 18:55:14 -0300

    Full Keys

--- keys.cpp
+++ keys.cpp
@@ -26,4 +26,58 @@ if (digitalRead(DpadUp) == HIGH) {
     } else {
         Keyboard.press('d');
     }
+
+    if (digitalRead(buttonX) == HIGH) {
+        Keyboard.release('x');
+    } else {
+        Keyboard.press('x');
+    }
+
+    if (digitalRead(buttonB) == HIGH) {
+        Keyboard.release('b');
+    } else {
+        Keyboard.press('b');
+    }
+
+    if (digitalRead(buttonY) == HIGH) {
+        Keyboard.release('y');
+    } else {
+        Keyboard.press('y');
+    }
+
+    if (digitalRead(buttonA) == HIGH) {
+        Keyboard.release('a');
+    } else {
+        Keyboard.press('a');
+    }
+
+    if (digitalRead(LTrigger) == HIGH) {
+        Keyboard.release('1');
+    } else {
+        Keyboard.press('1');
+    }
+
+    if (digitalRead(LShoulder) == HIGH) {
+        Keyboard.release('2');
+    } else {
+        Keyboard.press('2');
+    }
+
+    if (digitalRead(RTrigger) == HIGH) {
+        Keyboard.release('3');
+    } else {
+        Keyboard.press('3');
+    }
+
+    if (digitalRead(RShoulder) == HIGH) {
+        Keyboard.release('4');
+    } else {
+        Keyboard.press('4');
+    }
+
+    if (digitalRead(startButton) == HIGH) {
+        Keyboard.release('5');
+    } else {
+        Keyboard.press('5');
+    }
 }
