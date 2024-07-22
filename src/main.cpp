ec10f242e2240b2a12d277711a9d105580dfef4d
VitorTavaresO <vitortavares.o55@gmail.com>
2024-07-22 14:04:30 -0300

    Test of BlackButton Input

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,36 @@
 
 Joystick_ Joystick;
 
-const int pinToButtonMap = 15;
+const int blackButton = 2;
+const int whiteButton = 3;
+const int greenButton = 4;
+const int redButton = 5;
+const int yellowButton = 6;
+const int blueButton = 7;
 
 void setup()
 {
-  pinMode(pinToButtonMap, INPUT_PULLUP);
+  pinMode(blackButton, INPUT_PULLUP);
+  pinMode(whiteButton, INPUT_PULLUP);
+  pinMode(greenButton, INPUT_PULLUP);
+  pinMode(redButton, INPUT_PULLUP);
+  pinMode(yellowButton, INPUT_PULLUP);
+  pinMode(blueButton, INPUT_PULLUP);
   Joystick.begin();
+  Serial.begin(9600);
 }
 
-int lastButtonState = 0;
+int lastBlackButtonState = 0;
 
 void loop()
 {
 
-  int currentButtonState = !digitalRead(pinToButtonMap);
-  if (currentButtonState != lastButtonState)
+  int currentBlackButtonState = !digitalRead(blackButton);
+  if (currentBlackButtonState != lastBlackButtonState)
   {
-    Joystick.setButton(0, currentButtonState);
-    lastButtonState = currentButtonState;
+    Joystick.setButton(0, currentBlackButtonState);
+    lastBlackButtonState = currentBlackButtonState;
+    Serial.println("Black button pressed");
   }
 
   delay(50);
